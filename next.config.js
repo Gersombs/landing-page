78a8b68be3c6841d7f1cbf09a198508dd4d87ca2
Gersombs <gersombs@gmail.com>
2025-05-07 21:24:25 -0600

    Add next.config.js with experimental appDir configuration

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Si estás usando App Router, asegúrate de activar appDir
+    experimental: {
+      appDir: true
+    }
+    // aquí puedes agregar cualquier otra configuración que necesites
+  };
+  
+  module.exports = nextConfig;
+  
